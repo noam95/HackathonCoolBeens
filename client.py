94dd56be217b89a78fa12ac044ebbe0f72695ef2
noam <kotlar@post.bgu.ac.il>
2020-12-30 00:43:44 +0200

    starting git

--- client.py
+++ client.py
@@ -4,45 +4,86 @@
 ● Game mode - collect characters and from the keyboard and send them over TCP. collect
 data from the network and print it onscreen
 '''
-
+import msvcrt
 import socket
+import struct
+import datetime, time
 
 
-def Main():
-    # local host IP '127.0.0.1'
-    host = '192.168.0.1'
+# local host IP '127.0.0.1'
+import time
 
-    # Define the port on which you want to connect
-    port = 12345
+host = '127.0.0.1'
 
+# Define the port on which you want to connect
+port = 13117
+def tcpState(Tcp_Port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     # connect to server on local computer
-    s.connect((host, port))
-
+    s.connect((host, Tcp_Port))
+    #m=s.recv(1028)
     # message you send to server
     message = "shaurya says geeksforgeeks"
-    while True:
-
-        # message sent to server
-        s.send(message.encode('ascii'))
-
-        # messaga received from server
-        data = s.recv(1024)
-
-        # print the received message
-        # here it would be a reverse of sent message
-        print('Received from the server :', str(data.decode('ascii')))
-
-        # ask the client whether he wants to continue
-        ans = input('\nDo you want to continue(y/n) :')
-        if ans == 'y':
-            continue
-        else:
-            break
-    # close the connection
+    team_name = "Korkifix"
+    # message sent to server
+    s.send(team_name.encode())
+    # s.send(b'ascasd')
+    # messaga received from server
+    s.settimeout(40)
+    try:
+        welcomeMSG = s.recv(1024)
+        welcomeMSG = welcomeMSG.decode()
+        print(welcomeMSG)
+        s.send(b'ack')
+        groups = s.recv(1024)
+        groups = groups.decode()
+        print(groups)
+        s.send(b'ack')
+        starttype = s.recv(1024)
+        starttype = starttype.decode()
+        print(starttype)
+        # game start
+        then = datetime.datetime.now() + datetime.timedelta(seconds=10)
+        g = msvcrt.getch()
+        while then > datetime.datetime.now():
+            # time.sleep(1)
+            print("s")
+            s.send(g)  # check if this line sends key pressing
+        print("timeFinish")
+        try:
+            winner = s.recv(1024)
+            winner =winner.decode()
+            print(winner)
+        except:
+            print("error happend while waiting for winner massege")
+        # game end
+    except:
+        print("error happend while playing")
+        # close the connection
     s.close()
 
-
-if __name__ == '__main__':
-    Main()
+def udpState():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPROTO_UDP)
+    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    TCP_PORT = 0
+    udp_socket.bind(("",port))
+    print("Client started, listening for offer requests...")
+    x = True
+    #udp_socket.settimeout(20)
+    try:
+        while x:
+            buffer = udp_socket.recv(1024)
+            unPackMsg = struct.unpack('Ibh', buffer)
+            if unPackMsg[0] == 0xfeedbeef and unPackMsg[1] == 0x2:
+                x = False
+        TCP_PORT = unPackMsg[2]
+        iphost= host
+        print(f"Received offer from {iphost}, attempting to connect...")
+    except:
+        print("can't connet to server")
+    udp_socket.close()
+    return TCP_PORT
+while True:
+    Tcp_Port = udpState()
+    if Tcp_Port != 0:
+        tcpState(Tcp_Port)
--- server.py
+++ server.py
@@ -5,67 +5,221 @@ and new TCP connections. You leave this state after 10 seconds.
 state after 10 seconds.
 '''
 
-# import socket programming library
-import socket
+import socket, threading
+import struct
+import time
+from pip._vendor.distlib.compat import raw_input
+from concurrent.futures import ThreadPoolExecutor
+import datetime, time
 
-# import thread module
-from _thread import *
-import threading
+class game:
+    team1 = []
+    team2 = []
+    bol = True
+    counter1 = 0
+    counter2 = 0
 
-print_lock = threading.Lock()
+
+    def assignToTeam(self, player):
+        gameClasslock.acquire(True)
+        if self.bol:
+            self.team1.append(player)
+            self.bol = False
+            gameClasslock.release()
+            return "team1"
+        else:
+            self.team2.append(player)
+            self.bol = True
+            gameClasslock.release()
+            return "team2"
+
+    def getGroupsMsg(self):
+        msg ="group1\n"
+        for i in self.team1:
+            msg += i + "\n"
+        msg += "group2\n"
+        for i in self.team2:
+            msg += i + "\n"
+        return msg
+
+    def updateScore(self, counter, grupName):
+        gameClasslock.acquire(True)
+        if (grupName == "team1"):
+            self.counter1 += counter
+        else:
+            self.counter2 += counter
+        gameClasslock.release()
+
+    def calculateScore(self):
+        msg= "game over\ngroup1 get"+ str(self.counter1) +"points.\n"
+        msg += "group2 get" + str(self.counter2) + "points\n"
+        msg += "and the winer is......."
+        if self.counter1> self.counter2:
+            msg += str(self.team1)
+        if self.counter1< self.counter2:
+            msg+= str(self.team2)
+        else:
+            msg+= "tekooooo\n"
+        msg += "Congratulations!!!!!!!!"
+        return msg
+    def reset(self):
+        self.team1 = []
+        self.team2 = []
+        self.bol = True
+        self.counter1 = 0
+        self.counter2 = 0
 
 
-# thread function
-def threaded(c):
-    while True:
+LOCALHOST = "127.0.0.1"
+UDP_PORT = 13117
+SERVER_TCP_PORT = 7000
+gameClasslock = threading.Lock()
+getMessageLock = threading.Lock()
+sendScoreLock = threading.Lock()
+threads = []
 
-        # data received from client
-        data = c.recv(1024)
-        if not data:
-            print('Bye')
-
-            # lock released on exit
-            print_lock.release()
-            break
-
-        # reverse the given string from client
-        data = data[::-1]
-
-        # send back reversed string to client
-        c.send(data)
-
-        # connection closed
-    c.close()
+game1 = game()
 
 
 def Main():
-    host = ""
-
-    # reverse a port on your computer
-    # in our case it is 12345 but it
-    # can be anything
-    port = 12345
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    print("socket binded to port", port)
-
-    # put the socket into listening mode
-    s.listen(5)
-    print("socket is listening")
-
-    # a forever loop until client wants to exit
     while True:
-        # establish connection with client
-        c, addr = s.accept()
+        getMessageLock.acquire()
+        sendScoreLock.acquire()
+        udpthread = udpThraed()
+        print("udpThred start")
+        udpthread.start()
+        tcpthread = tcpConnection()
+        print("tcp thread start")
+        tcpthread.start()
+        tcpthread.join(10)
+        udpthread.join(10)
+        print("udp/tcp thread finished")
+        if(len(threads) != 0 ):
+            # groupsMsg = game1.getGroupsMsg()
+            getMessageLock.release()#start game
+            time.sleep(12)
+            print("game Finished")
+            sendScoreLock.release()#clculating score
+            time.sleep(10)
+            game1.reset()
+        else:
+            getMessageLock.release()
+            sendScoreLock.release()
 
-        # lock acquired by client
-        print_lock.acquire()
-        print('Connected to :', addr[0], ':', addr[1])
+    #close all the threads
+    #reset game
 
-        # Start a new thread and return its identifier
-        start_new_thread(threaded, (c,))
-    s.close()
+class udpThraed(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        udpState()
 
+class tcpConnection(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        tcp_state()
+
+class ClientThread(threading.Thread):
+    groupName = ""
+    def __init__(self, clientAddress, clientsocket):
+        threading.Thread.__init__(self)
+        self.clientAddress = clientAddress
+        self.csocket = clientsocket
+
+    def run(self):
+        self.csocket.settimeout(40)#case no msg recived
+        data = self.csocket.recv(2048)
+        clientname = data.decode()
+        self.groupName = game1.assignToTeam(clientname)
+        while(getMessageLock.locked()):
+            pass
+        val = self.startGameMassge()
+        if not val:
+            return
+        while(sendScoreLock.locked()):
+            pass
+        val = self.sendScore()
+        if not val:
+            return
+
+    def startGameMassge(self):
+        try:
+            msg = 'Welcome to Keyboard Spamming Battle Royale.'
+            self.csocket.send(msg.encode())
+            self.csocket.recv(1024)
+            groupsMsg = game1.getGroupsMsg()
+            self.csocket.send(groupsMsg.encode())
+            self.csocket.recv(1024)
+            startMsg = 'Start pressing keys on your keyboard as fast as you can!!'
+            self.csocket.send(startMsg.encode())
+
+            counter_game = 0
+            then = datetime.datetime.now() + datetime.timedelta(seconds=10)
+            try:
+                while then > datetime.datetime.now():
+
+                    self.csocket.settimeout(10)
+                    if self.csocket.recv(1024):
+                        counter_game += 1
+            except:
+                print("fail in getting typing from client")
+                return False
+
+            game1.updateScore(counter_game, self.groupName)
+            print(counter_game)
+        except:
+            print("client lost connection")
+            return False
+    def sendScore(self):
+        try:
+            msg = game1.calculateScore()
+            self.csocket.send(msg.encode())
+        except:
+            print("client connection lost")
+            return False
+
+
+
+
+        #game1.assignToTeam(groupName)
+        # msg = ''
+        #     if msg == 'bye':
+        #         break
+        #     print("from client", msg)
+        #     self.csocket.send(bytes(msg, 'UTF-8'))
+        # print("Client at ", self.clientAddress, " disconnected...")
+
+
+def tcp_state():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((LOCALHOST, SERVER_TCP_PORT))
+    while True:
+        server.listen(10)
+        clientsock, clientAddress = server.accept()
+        newthread = ClientThread(clientAddress, clientsock)
+        newthread.start()
+        threads.append(newthread)
+
+def udpState():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    #pack the udp format with struct
+    udp_offer_msg = struct.pack('Ibh', 0xfeedbeef, 0x2, SERVER_TCP_PORT)
+
+    print("Server started,listening on IP address 172.1.0.1")
+    then = datetime.datetime.now() + datetime.timedelta(seconds=10)
+    while then > datetime.datetime.now():
+        udp_socket.sendto(udp_offer_msg, ('<broadcast>', UDP_PORT))
+        time.sleep(1)
+
+    #closing the udp thread after 10 seconds of offering messages.
+    udp_socket.close()
 
 if __name__ == '__main__':
     Main()
+
